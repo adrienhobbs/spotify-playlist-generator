91dbb3bdd7dfbf6c72efec2ac9c59bb3b3015c3a
Adrien Hobbs <adrien.hobbs@gmail.com>
2019-09-06 17:39:38 -0400

    remove unnecessary code

--- src/axiosSetup.js
+++ src/axiosSetup.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 import firebase from "firebase/app";
-import Store from "./store";
 
 axios.interceptors.request.use(
   request => {
-    // Do something with response data
+    request.withCredentials = true;
     if (firebase.auth().currentUser) {
       return firebase
         .auth()
         .currentUser.getIdToken(true)
         .then(idToken => {
           request.headers.common["Authorization"] = `Bearer ${idToken}`;
-          request.withCredentials = true;
           return request;
         });
     } else {
@@ -20,18 +18,6 @@ axios.interceptors.request.use(
   },
   error => {
     // redirect to lgoin?
-    return Promise.reject(error);
-  }
-);
-
-axios.interceptors.response.use(
-  response => {
-    // Do something with response data
-    return response;
-  },
-  error => {
-    // redirect to lgoin?
-    Store.dispatch("logout");
     return Promise.reject(error);
   }
 );

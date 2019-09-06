cc17495da76d4bf908e25cd1b10798201b9ba17e
Adrien Hobbs <adrien.hobbs@gmail.com>
2019-09-06 17:40:00 -0400

    extract localstorage into own module

---
+++ src/storage.js
@@ -0,0 +1,58 @@
+import api from "./api";
+const msInDay = 86400000;
+const msToMins = ms => Math.round(ms * 0.00001666667);
+
+const log = msg =>
+  console.log(`%c  ${msg}  `, "background-color: #b96306; color: white;");
+
+const shouldUpdate = lastUpdated =>
+  log(msToMins(Date.now() - lastUpdated) + " mins since last update.") ||
+  Date.now() - lastUpdated > msInDay;
+
+const setItem = (prop, item) =>
+  localStorage.setItem(prop, JSON.stringify(item));
+
+const getItem = prop => JSON.parse(localStorage.getItem(prop));
+
+export default (function() {
+  const lastUpdated = getItem("lastUpdated");
+  const listeningDataKeys = [
+    "tracks",
+    "artists",
+    "recentlyPlayed",
+    "lastUpdated"
+  ];
+
+  const getListeningDataFromLocalStorage = () => {
+    return new Promise(resolve => {
+      let listeningData = {};
+      listeningDataKeys.forEach(key => (listeningData[key] = getItem(key)));
+      resolve(listeningData);
+    });
+  };
+
+  const setListeningDataToLocalStorage = data => {
+    Object.keys(data).forEach(key => {
+      setItem(key, data[key]);
+    });
+    return getListeningDataFromLocalStorage();
+  };
+
+  const getListeningData = (forceUpdate = false) => {
+    if (forceUpdate || !lastUpdated || shouldUpdate(lastUpdated)) {
+      log("Get Listening Data from API!");
+      return api.getListeningData().then(res => {
+        setItem("lastUpdated", Date.now());
+        return setListeningDataToLocalStorage(res.data);
+      });
+    } else {
+      log("Retrieving Listening Data from localstorage!");
+      return getListeningDataFromLocalStorage();
+    }
+  };
+
+  return {
+    getListeningData,
+    lastUpdated
+  };
+})();

2369f340acb1aec90f4bc0d1165d7f810c3aaab5
Adrien Hobbs <adrien.hobbs@gmail.com>
2019-09-06 13:35:31 -0400

    rudimentary display listening data

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,28 +1,55 @@
 <template>
   <div>
     <button @click="logout">logout</button>
-    <button @click="refresh">refresh</button>
     dashboard
+    <h1>short term artists</h1>
+    <ul>
+      <li
+        v-for="artist in $store.state.listeningData.artists.short_term"
+        :key="artist.id"
+      >
+        {{ artist.name }}
+      </li>
+    </ul>
+    <h1>medium term artists</h1>
+    <ul>
+      <li
+        v-for="artist in $store.state.listeningData.artists.medium_term"
+        :key="artist.id"
+      >
+        {{ artist.name }}
+      </li>
+    </ul>
+    <h1>long term artists</h1>
+    <ul>
+      <li
+        v-for="artist in $store.state.listeningData.artists.long_term"
+        :key="artist.id"
+      >
+        {{ artist.name }}
+      </li>
+    </ul>
+    <h1>Recently Played</h1>
+    <ul>
+      <li
+        v-for="recent in $store.state.listeningData.recentlyPlayed"
+        :key="recent.playedAt"
+      >
+        {{ recent.name }} <br />
+        {{ recent.artists[0].name }} <br />
+        {{ new Date(recent.playedAt) }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
   name: "UserDashboard",
   mounted() {},
   methods: {
     logout() {
       this.$store.dispatch("logout");
-    },
-    refresh() {
-      axios
-        .get("https://us-central1-play-gen.cloudfunctions.net/api/top")
-        .then(
-          res =>
-            console.log(res) ||
-            localStorage.setItem("tracks", JSON.stringify(res.data))
-        );
     }
   }
 };

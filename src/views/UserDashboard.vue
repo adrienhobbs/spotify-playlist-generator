ebb0cb47839005937e07cda461332035819c6656
Adrien Hobbs <adrien.hobbs@gmail.com>
2019-12-27 10:46:07 -0500

    group all time_ranges together

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -23,34 +23,35 @@
       </div>
     </div>
     <div v-if="selected === 'artists'" class="artists">
-      <div class="time-range" v-for="(range, key) in artists" :key="key">
-        <h1>{{ key }}</h1>
-        <div class="horizontal-wrapper">
-          <SelectableArtist
-            v-for="artist in range"
-            :artist="artist"
-            :key="artist.id"
-          />
-        </div>
+      <!-- <div class="time-range" v-for="(range, key) in artists" :key="key">
+        <h1>{{ key }}</h1> -->
+      <div class="vertical-wrapper">
+        <SelectableArtist
+          v-for="artist in allArtists"
+          :artist="artist"
+          :key="artist.id"
+        />
       </div>
+      <!-- </div> -->
     </div>
     <div v-else-if="selected === 'tracks'" class="tracks">
-      <div class="time-range" v-for="(range, key) in tracks" :key="key">
-        <h1>{{ key }}</h1>
-        <div class="horizontal-wrapper">
-          <SelectableTrack
-            v-for="track in range"
-            :track="track"
-            :key="track.playedAt"
-          />
-        </div>
+      <!-- <div class="time-range" v-for="(range, key) in tracks" :key="key"> -->
+      <!-- <h1>{{ key }}</h1> -->
+      <div class="vertical-wrapper">
+        <SelectableTrack
+          v-for="track in allTracks"
+          :track="track"
+          :key="track.playedAt"
+        />
       </div>
+      <!-- </div> -->
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import uniqBy from "lodash.uniqby";
 import SelectableTrack from "../components/SelectableTrack";
 import SelectableArtist from "../components/SelectableArtist";
 
@@ -62,7 +63,9 @@ export default {
   },
   data() {
     return {
-      selected: "tracks"
+      selected: "artists",
+      allTracks: [],
+      allArtists: []
     };
   },
   computed: {
@@ -70,6 +73,34 @@ export default {
       artists: "listeningData/artists",
       tracks: "listeningData/tracks"
     })
+  },
+  mounted() {
+    const allTracks = uniqBy(
+      [
+        ...this.tracks.recent,
+        ...this.tracks.short_term,
+        ...this.tracks.medium_term,
+        ...this.tracks.long_term
+      ],
+      "id"
+    );
+
+    const allArtists = uniqBy(
+      [
+        ...this.artists.short_term,
+        ...this.artists.medium_term,
+        ...this.artists.long_term
+      ],
+      "id"
+    );
+
+    this.allTracks = allTracks.sort((trackA, trackB) => {
+      return trackA.popularity < trackB.popularity;
+    });
+
+    this.allArtists = allArtists.sort((artistA, artistB) => {
+      return artistA.popularity < artistB.popularity;
+    });
   },
   methods: {
     logout() {
@@ -91,9 +122,60 @@ export default {
 .container {
   max-width: 940px;
   margin: auto;
+  margin-bottom: 60px;
   @media (max-width: 940px) {
     padding-left: 30px;
     padding-right: 30px;
+  }
+}
+
+.vertical-wrapper {
+  background: #1d1d1d;
+  border-radius: 15px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+
+.artist,
+.track {
+  margin-bottom: 15px;
+  display: flex;
+  position: relative;
+  background-color: #2b2b2b;
+  padding: 10px 15px 10px 15px;
+  border-radius: 8px;
+  align-items: center;
+
+  &.selected {
+    background-color: #fff;
+  }
+
+  a {
+    text-decoration: none;
+    color: #b3b3b3;
+
+    &:hover {
+      color: white;
+      text-decoration: underline;
+    }
+  }
+
+  img {
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+    object-fit: cover;
+    box-shadow: 0 10px 30px 0 rgba(0, 0, 0, 0.3), 0 1px 2px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  .name {
+    text-align: center;
   }
 }
 
@@ -102,47 +184,17 @@ export default {
   list-style: none;
   overflow-x: scroll;
   scroll-snap-type: x mandatory;
-
-  .artist,
-  .track {
-    margin-right: 15px;
-    margin-bottom: 15px;
-    display: flex;
-    align-items: center;
-    position: relative;
-    flex-direction: column;
-    scroll-snap-align: start;
-    background-color: #2b2b2b;
-    padding: 20px 20px 16px;
-    border-radius: 8px;
-
-    &:last-of-type {
-      margin-right: 30px;
-    }
-
-    img {
-      width: 161px;
-      height: 161px;
-      object-fit: cover;
-      box-shadow: 0 10px 30px 0 rgba(0, 0, 0, 0.3),
-        0 1px 2px 0 rgba(0, 0, 0, 0.2);
-    }
-
-    .name {
-      color: #b3b3b3;
-      text-align: center;
-      padding-top: 10px;
-    }
-  }
 }
 
 .toggle {
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-bottom: 30px;
 }
 
 .toggle-inner {
+  box-shadow: 0 10px 30px 0 rgba(0, 0, 0, 0.3), 0 1px 2px 0 rgba(0, 0, 0, 0.2);
   display: inline-flex;
   align-items: center;
   justify-content: center;
